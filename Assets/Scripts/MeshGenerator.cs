68bc930e2692602ea3bfff5e34095ada2d93d6f6
MrFurlan98 <viniciusrf1998@hotmail.com>
2018-09-12 20:33:52 -0300

    add rotation manipulation

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -25,13 +25,14 @@ public class MeshGenerator : MonoBehaviour {
     }
     public void SubVertices(List<Vector3> pVertices)
     {
-        vertices = pVertices;
+        //vertices = pVertices;
     }
     // Use this for initialization
     void Start() {
 
 
-        //vertices = GeneratePositions(verticeAmount);
+      //  vertices = GeneratePositions(verticeAmount);
+      //  OrganicModel(27, 27, 27, 0.8f);
         //triangules = new int[(vertices.Count - 2) * 3];
     }
  
@@ -65,22 +66,31 @@ public class MeshGenerator : MonoBehaviour {
     List<Vector3> GeneratePositions(int vertices)
     {
         List<Vector3> tempPositions = new List<Vector3>();
-        for(int i = 0; i < vertices; i++)
+
+        for(float i = -vertices; i <= vertices; i +=0.45f)
         {
-            float x = i * 0.3f;
-            Vector3 position = Vector3.zero;
-            if(i<=3)
+            float j = Mathf.Sqrt(Mathf.Abs(Mathf.Pow(i, 2) - Mathf.Pow(vertices, 2)));
+            tempPositions.Add(new Vector3(i, j, 0));
+        }
+        for (float i = vertices; i >= -vertices; i-=0.45f)
+        {
+            float j = Mathf.Sqrt(Mathf.Abs(Mathf.Pow(i, 2) - Mathf.Pow(vertices, 2)));
+            tempPositions.Add(new Vector3(i, -j, 0));
+        }
+        tempPositions.Sort((a, b) => a.x.CompareTo(b.x));
+
+        for(int i =0; i<tempPositions.Count;i++)
+        {
+            if(i%2==0)
             {
-                position.x =x;
-                position.y = Mathf.Pow(x, 3);
+                tempPositions[i] = new Vector3(tempPositions[i].x,-Mathf.Abs(tempPositions[i].y));
             }
             else
             {
-                position.x = 3*0.3f;
-                position.y = -x+1.5f;
+                tempPositions[i] = new Vector3(tempPositions[i].x, Mathf.Abs(tempPositions[i].y));
             }
-            tempPositions.Add(position);
         }
+
         return tempPositions;
     }
     int CalcTriangle(int vertices)
@@ -111,17 +121,96 @@ public class MeshGenerator : MonoBehaviour {
         }
         return tempTriangles.ToArray();
     }
-	// Update is called once per frame
+    void OrganicModel(int lastIndex, int index ,int originIndex, float mult)
+    {
+        int[] indexesNearest = IndexNearest(index);
+        if(mult >= 0.25f)
+        {
+            Vector3 direction = ((vertices[lastIndex] - vertices[index]).normalized) * mult;
+            vertices[index] += direction;
+            if(indexesNearest[0] == originIndex && indexesNearest[1] == originIndex)
+            {
+                OrganicModel(originIndex, indexesNearest[0], originIndex, mult / 1.8f);
+                OrganicModel(originIndex, indexesNearest[1], originIndex, mult / 1.8f);
+            }
+            else
+            {
+                if(indexesNearest[0] != lastIndex)
+                {
+                    OrganicModel(originIndex, indexesNearest[0], originIndex, mult / 1.8f);
+
+                }
+                if (indexesNearest[1] != lastIndex)
+                {
+                    OrganicModel(originIndex, indexesNearest[1], originIndex, mult / 1.8f);
+
+                }
+            }
+        }
+        
+            
+    }
+
+    int[] IndexNearest(int index)
+    {
+        int[] indexes = new int[2];
+        float menorValor = 0;
+        float menorValor2 = 0;
+        if (index!=0)
+        {
+            menorValor = Vector3.Distance(vertices[index], vertices[0]);
+            indexes[0] = 0;
+            menorValor2 = Vector3.Distance(vertices[index], vertices[0]);
+            indexes[1] = 0;
+        }
+        else
+        {
+            menorValor = Vector3.Distance(vertices[index], vertices[1]);
+            indexes[0] = 1;
+            menorValor2 = Vector3.Distance(vertices[index], vertices[1]);
+            indexes[1] = 1;
+        } 
+        for (int i=0;i<vertices.Count;i++)
+        {
+            if(Vector3.Distance(vertices[index],vertices[i]) < menorValor && index != i)
+            {
+                menorValor = Vector3.Distance(vertices[index], vertices[i]);
+
+                indexes[0] = i;
+            }
+        }
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            if (Vector3.Distance(vertices[index], vertices[i]) < menorValor2 && index != i && indexes[0] != i)
+            {
+                menorValor2 = Vector3.Distance(vertices[index], vertices[i]);
+
+                indexes[1] = i;
+            }
+        }
+        return indexes;
+    }
+     // Update is called once per frame
 	void Update () {
         CreateMesh();
+      
     }
+   
 
     private void OnDrawGizmosSelected()
     {
+        
         Gizmos.color = Color.red;
-        for(int i = 0; i < vertices.Count; i++)
+        
+        for (int i = 0; i < vertices.Count; i++)
         {
-            Gizmos.DrawSphere(vertices[i], 0.1f);
+
+            float radius = (Mathf.Abs(vertices[i].x)/vertices[i].x) * Vector3.Distance(transform.position, vertices[i]);
+            float arcTg = Mathf.Atan(vertices[i].y / vertices[i].x);
+            Debug.Log(arcTg);
+            Vector3 rotation = new Vector3(radius * Mathf.Cos((transform.localEulerAngles.z * Mathf.Deg2Rad) + arcTg) , radius * Mathf.Sin( (transform.localEulerAngles.z * Mathf.Deg2Rad)+ arcTg));
+            Gizmos.DrawSphere(transform.position + rotation , 0.1f);
+
         }
     }
 }
