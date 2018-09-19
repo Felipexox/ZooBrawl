ee99837f163d25f7c4b3ebf0a6e582d5b5f1403c
Felipexox <felipeaa100@gmail.com>
2018-09-18 22:45:29 -0300

    [Commit]   - Fix height and width

--- Assets/Scripts/Torso.cs
+++ Assets/Scripts/Torso.cs
@@ -13,33 +13,29 @@ public class Torso : MonoBehaviour
     private float height;
     [SerializeField]
     public List<Vector3> vertices = new List<Vector3>();
-    public float teste;
 
     [SerializeField]
     private MeshGenerator mesh;
 
     private void Start()
     {
-        teste = 1;
-        mesh.Vertices = vertices;
+        mesh.Vertices = new List<Vector3>(vertices);
     }
     private void Update()
     {
-        if(teste!=height)
-        {
-            mesh.vertices = HeightNLength(height,length);
-        }
-        teste = height;
-        mesh.CreateMesh();
+      
+        HeightNLength(height,length);
+        
+     
+     
     }
-    private List<Vector3> HeightNLength(float height,float length)
+    private void HeightNLength(float height,float length)
     {
         
         for (int i =0;i<4;i++)
         {
-            vertices[i] = new Vector3(vertices[i].x*length, vertices[i].y * height);
+            mesh.vertices[i] = new Vector3(vertices[i].x*length, vertices[i].y * height);
         }
-        return vertices;
     }
 
 }
